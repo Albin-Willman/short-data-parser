f77a7358289a9e70a5605ca91e42cd34893afd9d
Albin <j.albin.willman@gmail.com>
2017-02-28 15:57:08 +0100

    Save chart before tweet

--- lib/utilities/ChartGenerator.rb
+++ lib/utilities/ChartGenerator.rb
@@ -1,21 +1,22 @@
 require 'googlecharts'
 class ChartGenerator
+  CHART_NAME = 'tmp/chart.png'
   def self.build_company_chart(company)
     data, legends = [], []
     company[:positions].each do |key, p|
       legends << p[:name]
       data << p[:positions].values.map { |v| v*100 }
     end
-    chart = Gchart.new( type: 'line',
-                    title: company[:name],
-                    theme: :pastel,
-                    data: data,
-                    legend: legends,
-                    axis_with_labels: ['Date', '%'],
-                    size: '600x300',
-                    filename: 'tmp/chart.png')
+    Gchart.new( type: 'line',
+                title: company[:name],
+                theme: :pastel,
+                data: data,
+                legend: legends,
+                axis_with_labels: ['Date', '%'],
+                size: '600x300',
+                filename: CHART_NAME).file
 
     # Record file in filesystem
-    "#{Rails.root.to_s}/tmp/chart.png"
+    "#{Rails.root.to_s}/#{CHART_NAME}"
   end
 end
